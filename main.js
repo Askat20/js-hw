55d833b103905ed4edf2ee04db25a08f8e2ef285
askat <saatbek493@gmail.com>
2024-07-17 12:06:35 +0300

    finished

---
+++ main.js
@@ -0,0 +1,48 @@
+let firstUserName = "Nooruz";
+console.log(firstUserName);
+let secondUserName = "Zhakshylyk";
+console.log(secondUserName);
+let thirdUserName = "Alisher";
+console.log(thirdUserName);
+let fourthUserName = "Elizar";
+console.log(fourthUserName);
+let firstUserPoint = "97";
+console.log(firstUserPoint);
+let secondUserPoint = "89";
+console.log(secondUserPoint);
+let thirdUserPoint = "75";
+console.log(thirdUserPoint);
+let fourthUserPoint = "90";
+console.log(fourthUserPoint);
+const result = "Экзамендин катышуучулары жана результаттары:";
+console.log(result);
+let interpolation = "Nooruz " + firstUserPoint + " баллов из 100 ";
+console.log(interpolation);
+let interpolation2 = "Zhakshylyk " + secondUserPoint + " баллов из 100 ";
+console.log(interpolation2);
+let interpolation3 = "Alisher " + thirdUserPoint + " баллов из 100 ";
+console.log(interpolation3);
+let interpolation4 = "ELizar " + fourthUserPoint + " баллов из 100 ";
+console.log(interpolation4);
+let number = 4;
+console.log(`${1} х ${number} = ${number * 1}`);
+console.log(`${2} x ${number} = ${number * 2}`);
+console.log(`${3} x ${number} = ${number * 3}`);
+console.log(`${4} x ${number} = ${number * 4}`);
+console.log(`${5} x ${number} = ${number * 5}`);
+console.log(`${6} x ${number} = ${number * 6}`);
+console.log(`${7} x ${number} = ${number * 7}`);
+console.log(`${8} x ${number} = ${number * 8}`);
+console.log(`${9} x ${number} = ${number * 9}`);
+console.log(`${10} x ${number} = ${number * 10}`);
+
+const info = alert("Вы успешно зарегистрировались!");
+console.log(info);
+
+const info2 = confirm(
+  "При перезагрузке сайта, введённые данные на форме будут очищены, Вы уверены что хотите перезагрузить?"
+);
+console.log(info2);
+
+const info3 = prompt("Введите ваше имя");
+console.log(info3);
